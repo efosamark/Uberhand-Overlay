69736c5ccdbd012827cbdb52d4b8f1df6831903e
Irne Racoonovich <IrneRacoonovich@outlook.com>
2023-09-09 16:30:36 +0300

    skip commands meant for different HW type

--- source/ini_funcs.hpp
+++ source/ini_funcs.hpp
@@ -171,6 +171,32 @@ std::map<std::string, std::map<std::string, std::string>> getParsedDataFromIniFi
     return parsedData;
 }
 
+bool isMarikoHWType()
+{
+    u64 hardware_type = -1;
+    auto rc = splGetConfig(SplConfigItem_HardwareType, &hardware_type);
+    if (R_FAILED(rc)) {
+        logMessage("ERROR: splGetConfig failed to fetch HardwareType");
+        return false;
+    }
+
+    logMessage("INFO: HardwareType: " + std::to_string(hardware_type));
+
+    switch (hardware_type) {
+    case 0: // Icosa
+    case 1: // Copper
+        return false; // Erista
+    case 2: // Hoag
+    case 3: // Iowa
+    case 4: // Calcio
+    case 5: // Aula
+        return true; // Mariko
+    default:
+        logMessage("ERROR: unknown HardwareType: " + std::to_string(hardware_type));
+        throw std::runtime_error("ERROR: unknown HardwareType: " + std::to_string(hardware_type));
+        return false;
+    }
+}
 
 std::vector<std::pair<std::string, std::vector<std::vector<std::string>>>> loadOptionsFromIni(const std::string& configIniPath, bool makeConfig = false) {
     std::vector<std::pair<std::string, std::vector<std::vector<std::string>>>> options;
@@ -199,6 +225,8 @@ std::vector<std::pair<std::string, std::vector<std::vector<std::string>>>> loadO
     char line[BufferSize];
     std::string currentOption;
     std::vector<std::vector<std::string>> commands;
+    static bool isMariko = isMarikoHWType();
+    bool skipCommand = false;
 
     while (fgets(line, sizeof(line), configFile)) {
         std::string trimmedLine = line;
@@ -207,12 +235,21 @@ std::vector<std::pair<std::string, std::vector<std::vector<std::string>>>> loadO
         if (trimmedLine.empty() || trimmedLine[0] == '#') {
             // Skip empty lines and comment lines
             continue;
+        } else if (trimmedLine == "; Mariko") {
+            skipCommand = (!isMariko);
+            continue;
+        } else if (trimmedLine == "; Erista") {
+            skipCommand = (isMariko);
+            continue;
         } else if (trimmedLine[0] == '[' && trimmedLine.back() == ']') {
             // New option section
             if (!currentOption.empty()) {
-                // Store previous option and its commands
-                options.emplace_back(std::move(currentOption), std::move(commands));
+                if(!skipCommand){
+                    // Store previous option and its commands
+                    options.emplace_back(std::move(currentOption), std::move(commands));
+                }
                 commands.clear();
+                skipCommand = false;
             }
             currentOption = trimmedLine.substr(1, trimmedLine.size() - 2);  // Extract option name
         } else {
